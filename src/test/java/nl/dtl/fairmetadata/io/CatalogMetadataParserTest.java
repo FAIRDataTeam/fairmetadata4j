cc5925e53b74651e3e53a9f126fec3c97cc90383
rkaliyaperumal <r.kaliyaperumal@lumc.nl>
2016-09-08 15:19:27 +0200

    More unit tests added

--- src/test/java/nl/dtl/fairmetadata/io/CatalogMetadataParserTest.java
+++ src/test/java/nl/dtl/fairmetadata/io/CatalogMetadataParserTest.java
@@ -5,17 +5,26 @@
  */
 package nl.dtl.fairmetadata.io;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import nl.dtl.fairmetadata.model.CatalogMetadata;
 import nl.dtl.fairmetadata.utils.ExampleFilesUtils;
+import nl.dtl.fairmetadata.utils.vocabulary.DCAT;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
+import org.openrdf.model.Statement;
 import org.openrdf.model.URI;
 import org.openrdf.model.impl.URIImpl;
+import org.openrdf.model.vocabulary.DCTERMS;
+import org.openrdf.model.vocabulary.RDFS;
 
 /**
  *
  * @author rajaram
  */
+//@Ignore
 public class CatalogMetadataParserTest {
 
     /**
@@ -23,6 +32,38 @@ public class CatalogMetadataParserTest {
      * @throws java.lang.Exception
      */
     private final CatalogMetadataParser parser = new CatalogMetadataParser();
+    
+    /**
+     * Test null RDF string, this test is expected to throw exception
+     * @throws Exception 
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testParseNullRDFString() throws Exception {
+        System.out.println("parse invalid catalog content");
+        URI cURI = new URIImpl(ExampleFilesUtils.CATALOG_URI);
+        URI fURI = new URIImpl(ExampleFilesUtils.FDP_URI);
+        parser.parse(null, ExampleFilesUtils.CATALOG_ID, cURI, fURI, 
+                ExampleFilesUtils.FILE_FORMAT);
+        fail("This test is execpeted to throw an error");
+    }
+    
+    /**
+     * Test empty RDF string, this test is expected to throw exception
+     * @throws Exception 
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testParseEmptyRDFString() throws Exception {
+        System.out.println("parse invalid catalog content");
+        URI cURI = new URIImpl(ExampleFilesUtils.CATALOG_URI);
+        URI fURI = new URIImpl(ExampleFilesUtils.FDP_URI);
+        parser.parse("", ExampleFilesUtils.CATALOG_ID, cURI, fURI, 
+                ExampleFilesUtils.FILE_FORMAT);
+        fail("This test is execpeted to throw an error");
+    }
+    /**
+     * Test valid catalog metadata rdf file
+     * @throws Exception 
+     */
     @Test
     public void testParseFile() throws Exception {
         System.out.println("parse valid catalog content");
@@ -33,6 +74,94 @@ public class CatalogMetadataParserTest {
                 ExampleFilesUtils.CATALOG_METADATA_FILE), 
                 ExampleFilesUtils.CATALOG_ID, cURI, fURI, 
                 ExampleFilesUtils.FILE_FORMAT);
+        assertNotNull(metadata);
+    }
+    
+    /**
+     * Test missing catalog rdf statement, this test is excepted to throw 
+     * an exception
+     * @throws Exception 
+     */
+    @Test(expected = MetadataExeception.class)
+    public void testParseNoVersionStatement() throws Exception {
+        System.out.println("Missing version statement");
+        List<Statement> stmts = ExampleFilesUtils.getFileContentAsStatements(
+                ExampleFilesUtils.CATALOG_METADATA_FILE, 
+                        ExampleFilesUtils.CATALOG_URI);
+        Iterator<Statement> it = stmts.iterator();
+        while(it.hasNext()) {
+            Statement st = it.next();
+            if(st.getPredicate().equals(DCTERMS.HAS_VERSION)) {
+                stmts.remove(st);
+                break;
+            }
+        }
+        URI cURI = new URIImpl(ExampleFilesUtils.CATALOG_URI);
+        parser.parse(stmts , cURI);
+       fail("This test is execpeted to throw an error");
+    }
+    
+    /**
+     * Test missing catalog rdf statement, this test is excepted to throw 
+     * an exception
+     * @throws Exception 
+     */
+    @Test(expected = MetadataExeception.class)
+    public void testParseNoTitleStatement() throws Exception {
+        System.out.println("Missing title statement");
+        List<Statement> stmts = ExampleFilesUtils.getFileContentAsStatements(
+                ExampleFilesUtils.CATALOG_METADATA_FILE, 
+                        ExampleFilesUtils.CATALOG_URI);
+        Iterator<Statement> it = stmts.iterator();
+        List<Statement> in = new ArrayList();
+        while(it.hasNext()) {
+            Statement st = it.next();
+            if(! st.getPredicate().equals(RDFS.LABEL) && 
+                    !st.getPredicate().equals(DCTERMS.TITLE)) {
+                in.add(st);
+            }
+        }
+        URI cURI = new URIImpl(ExampleFilesUtils.CATALOG_URI);
+        parser.parse(in , cURI);
+       fail("This test is execpeted to throw an error");
+    }
+    
+    /**
+     * Test missing catalog rdf statement, this test is excepted to throw 
+     * an exception
+     * @throws Exception 
+     */
+    @Test(expected = MetadataExeception.class)
+    public void testParseNoThemeTaxonomyStatement() throws Exception {
+        System.out.println("Missing title statement");
+        List<Statement> stmts = ExampleFilesUtils.getFileContentAsStatements(
+                ExampleFilesUtils.CATALOG_METADATA_FILE, 
+                        ExampleFilesUtils.CATALOG_URI);
+        Iterator<Statement> it = stmts.iterator();
+        List<Statement> in = new ArrayList();
+        while(it.hasNext()) {
+            Statement st = it.next();
+            if(! st.getPredicate().equals(DCAT.THEME_TAXONOMY)) {
+                in.add(st);
+            }
+        }
+        URI cURI = new URIImpl(ExampleFilesUtils.CATALOG_URI);
+        parser.parse(in , cURI);
+       fail("This test is execpeted to throw an error");
+    }
+    
+    /**
+     * Test valid catalog rdf statements
+     * @throws Exception 
+     */
+    @Test
+    public void testParseStatements() throws Exception {
+        System.out.println("parse valid catalog content");
+        URI cURI = new URIImpl(ExampleFilesUtils.CATALOG_URI);
+        CatalogMetadata metadata = parser.parse(
+                ExampleFilesUtils.getFileContentAsStatements(
+                ExampleFilesUtils.CATALOG_METADATA_FILE, 
+                        ExampleFilesUtils.CATALOG_URI), cURI);
         assertNotNull(metadata);
     }
     
