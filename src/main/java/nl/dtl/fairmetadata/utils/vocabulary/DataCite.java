ba5f376fa24a4ef5b25cd582aec47c76122da8fd
rkaliyaperumal <r.kaliyaperumal@lumc.nl>
2016-11-29 17:50:55 +0100

    DataCite ontology added

---
+++ src/main/java/nl/dtl/fairmetadata/utils/vocabulary/DataCite.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.dtl.fairmetadata.utils.vocabulary;
+
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.URIImpl;
+
+/**
+ * DataCite vocabulary.
+ * See {@link <a href="http://www.sparontologies.net/ontologies/datacite/source.html">DataCite Vocabulary</a>}.
+ * 
+ * @author Rajaram Kaliyaperumal
+ * @since 2016-11-29
+ * @version 0.1
+ */
+public class DataCite {
+     
+    public static final String PREFIX = "dataCite";        
+    public static final String NAMESPACE = "http://purl.org/spar/datacite/";
+    public static final URI IDENTIFIER = 
+            new URIImpl(NAMESPACE + "Identifier");
+}
