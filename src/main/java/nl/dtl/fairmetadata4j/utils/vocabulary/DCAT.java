2d4b7c4b0fd39b91ed1dd52fe4f616eb0e0137a9
rkaliyaperumal <r.kaliyaperumal@lumc.nl>
2017-02-10 12:48:32 +0100

    Package renamed

---
+++ src/main/java/nl/dtl/fairmetadata4j/utils/vocabulary/DCAT.java
@@ -0,0 +1,70 @@
+/**
+ * The MIT License
+ * Copyright © 2017 DTL
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nl.dtl.fairmetadata4j.utils.vocabulary;
+
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+
+
+
+
+/**
+ * DCAT vocabulary.
+ * See {@link <a href="https://www.w3.org/TR/vocab-dcat/">DCAT Vocabulary</a>}.
+ * 
+ * @author Rajaram Kaliyaperumal <rr.kaliyaperumal@gmail.com>
+ * @author Kees Burger <kees.burger@dtls.nl>
+ * @since 2016-09-06
+ * @version 0.1
+ */
+public class DCAT {
+    private static final ValueFactory f = SimpleValueFactory.getInstance();
+    public static final String PREFIX = "dcat";
+    public static final String NAMESPACE = "http://www.w3.org/ns/dcat#";
+    public static final IRI THEME_TAXONOMY = f.createIRI(NAMESPACE + "themeTaxonomy");
+    public static final IRI LANDING_PAGE = f.createIRI(NAMESPACE + "landingPage");
+    public static final IRI THEME = f.createIRI(NAMESPACE + "theme");
+    public static final IRI CONTACT_POINT = f.createIRI(NAMESPACE + "contactPoint");
+    public static final IRI KEYWORD = f.createIRI(NAMESPACE + "keyword");
+    public static final IRI TYPE_CATALOG = f.createIRI(NAMESPACE + "Catalog");
+    public static final IRI TYPE_DATASET = f.createIRI(NAMESPACE + "Dataset");
+    public static final IRI TYPE_DISTRIBUTION = f.createIRI
+        (NAMESPACE + "Distribution");
+    public static final IRI DATASET = f.createIRI(NAMESPACE + "dataset");
+    public static final IRI ACCESS_URL = f.createIRI(NAMESPACE + "accessURL");
+    public static final IRI DISTRIBUTION = f.createIRI
+        (NAMESPACE + "distribution");
+    public static final IRI DOWNLOAD_URL = f.createIRI
+        (NAMESPACE + "downloadURL");
+    public static final IRI MEDIA_TYPE = f.createIRI(NAMESPACE + "mediaType");
+    public static final IRI FORMAT = f.createIRI(NAMESPACE + "format");
+    public static final IRI BYTE_SIZE = f.createIRI(NAMESPACE + "byteSize");
+    
+}
---
+++ src/test/java/nl/dtl/fairmetadata4j/io/CatalogMetadataParserTest.java
@@ -0,0 +1,162 @@
+/**
+ * The MIT License
+ * Copyright © 2017 DTL
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.dtl.fairmetadata4j.io;
+
+import nl.dtl.fairmetadata4j.io.CatalogMetadataParser;
+import java.util.List;
+import nl.dtl.fairmetadata4j.model.CatalogMetadata;
+import nl.dtl.fairmetadata4j.utils.ExampleFilesUtils;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Statement;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for CatalogMetadataParser.
+ * 
+ * @author Rajaram Kaliyaperumal <rr.kaliyaperumal@gmail.com>
+ * @author Kees Burger <kees.burger@dtls.nl>
+ * @since 2016-09-08
+ * @version 0.1
+ */
+public class CatalogMetadataParserTest {
+    
+    private final CatalogMetadataParser parser = new CatalogMetadataParser();
+    
+    /**
+     * Test null RDF string, this test is expected to throw exception
+     * @throws Exception 
+     */
+    @Test(expected = NullPointerException.class)
+    public void testParseNullRDFString() throws Exception {
+        System.out.println("Test : Parse invalid catalog content");
+        IRI cURI = ExampleFilesUtils.CATALOG_URI;
+        IRI fURI = ExampleFilesUtils.FDP_URI;
+        parser.parse(null, cURI, fURI, ExampleFilesUtils.FILE_FORMAT);
+        fail("This test is execpeted to throw an error");
+    }
+    
+    /**
+     * Test empty RDF string, this test is expected to throw exception
+     * @throws Exception 
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testParseEmptyRDFString() throws Exception {
+        System.out.println("Test : Parse invalid catalog content");
+        IRI cURI = ExampleFilesUtils.CATALOG_URI;
+        IRI fURI = ExampleFilesUtils.FDP_URI;
+        parser.parse("", cURI, fURI, ExampleFilesUtils.FILE_FORMAT);
+        fail("This test is execpeted to throw an error");
+    } 
+    /**
+     * Test null RDFFormat, this test is expected to throw exception
+     * @throws Exception 
+     */
+    @Test(expected = NullPointerException.class)
+    public void testParseNullRDFFormat() throws Exception {
+        System.out.println("Test : Parse invalid catalog content");
+        IRI cURI = ExampleFilesUtils.CATALOG_URI;
+        IRI fURI = ExampleFilesUtils.FDP_URI;
+        parser.parse(ExampleFilesUtils.getFileContentAsString(
+                ExampleFilesUtils.CATALOG_METADATA_FILE), cURI, fURI, null);
+        fail("This test is execpeted to throw an error");
+    }
+    /**
+     * Test valid catalog metadata rdf file
+     * @throws Exception 
+     */
+    @Test
+    public void testParseFile() throws Exception {
+        System.out.println("Test : Parse valid catalog content");
+        IRI cURI = ExampleFilesUtils.CATALOG_URI;
+        IRI fURI = ExampleFilesUtils.FDP_URI;
+        CatalogMetadata metadata = parser.parse(
+                ExampleFilesUtils.getFileContentAsString(
+                ExampleFilesUtils.CATALOG_METADATA_FILE), cURI, fURI, 
+                ExampleFilesUtils.FILE_FORMAT);
+        assertNotNull(metadata);
+    }
+    
+    /**
+     * Test null catalog URI, this test is excepted to throw 
+     * an exception
+     * @throws Exception 
+     */
+    @Test(expected = NullPointerException.class)
+    public void testNullCatalogURI() throws Exception {
+        System.out.println("Test : Missing catalog URL");
+        List<Statement> stmts = ExampleFilesUtils.getFileContentAsStatements(
+                ExampleFilesUtils.CATALOG_METADATA_FILE, 
+                        ExampleFilesUtils.CATALOG_URI.toString());
+        parser.parse(stmts , null);
+        fail("This test is execpeted to throw an error");
+    }
+    
+    /**
+     * Test null statements, this test is excepted to throw 
+     * an exception
+     * @throws Exception 
+     */
+    @Test(expected = NullPointerException.class)
+    public void testNullStatements() throws Exception {
+        System.out.println("Test : Parse valid catalog content");
+        IRI cURI = ExampleFilesUtils.CATALOG_URI;
+        parser.parse(null, cURI);
+        fail("This test is execpeted to throw an error");
+    }
+    
+    /**
+     * Test valid catalog rdf statements
+     * @throws Exception 
+     */
+    @Test
+    public void testParseStatements() throws Exception {
+        System.out.println("Test : Parse valid catalog content");
+        IRI cURI = ExampleFilesUtils.CATALOG_URI;
+        CatalogMetadata metadata = parser.parse(
+                ExampleFilesUtils.getFileContentAsStatements(
+                ExampleFilesUtils.CATALOG_METADATA_FILE, 
+                        ExampleFilesUtils.CATALOG_URI.toString()), cURI);
+        assertNotNull(metadata);
+    }
+    
+    /**
+     * Test valid catalog metadata rdf file, with no base
+     * @throws Exception 
+     */
+    @Test
+    public void testParseFileWithNoBase() throws Exception {
+        System.out.println("Test : Parse valid catalog content with no base uri");
+        CatalogMetadata metadata = parser.parse(
+                ExampleFilesUtils.getFileContentAsString(
+                ExampleFilesUtils.CATALOG_METADATA_FILE), null,
+                ExampleFilesUtils.FILE_FORMAT);
+        assertNotNull(metadata);
+    }
+    
+}
